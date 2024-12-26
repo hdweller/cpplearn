d6bcabd3a28f0afb47838886a80234e41c65dc4c
hdweller <hilldweller@duck.com>
2024-12-26 13:27:11 -0500

    Add files via upload

---
+++ install-vscode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install the vscode product
+# prereq: use the prereq script first to get the repo definition on the system
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
+
